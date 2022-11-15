05b872c7c881053a2c2f0ce86e9bf39d68c3117f
Ashley Mathew <amathew195@gmail.com>
2022-11-15 14:27:58 -0600

    added reset button

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,8 +1,35 @@
+import _ from "underscore";
+import React, { useState } from "react";
+import "./EightBall.css";
+
+const ANSWERS = [
+  { msg: "It is certain.", color: "green" },
+  { msg: "It is decidedly so.", color: "green" },
+  { msg: "Without a doubt.", color: "green" },
+  { msg: "Yes - definitely.", color: "green" },
+  { msg: "You may rely on it.", color: "green" },
+  { msg: "As I see it, yes.", color: "green" },
+  { msg: "Most likely.", color: "green" },
+  { msg: "Outlook good.", color: "green" },
+  { msg: "Yes.", color: "green" },
+  { msg: "Signs point to yes.", color: "goldenrod" },
+  { msg: "Reply hazy, try again.", color: "goldenrod" },
+  { msg: "Ask again later.", color: "goldenrod" },
+  { msg: "Better not tell you now.", color: "goldenrod" },
+  { msg: "Cannot predict now.", color: "goldenrod" },
+  { msg: "Concentrate and ask again.", color: "goldenrod" },
+  { msg: "Don't count on it.", color: "red" },
+  { msg: "My reply is no.", color: "red" },
+  { msg: "My sources say no.", color: "red" },
+  { msg: "Outlook not so good.", color: "red" },
+  { msg: "Very doubtful.", color: "red" },
+];
+
 /**
  * Displaying a random answer in the eight ball
  *
  * Props:
- * - answers
+ * - answers: array of objects
  *
  * State:
  * - color: string
@@ -12,28 +39,40 @@
  *
  */
 
-import _ from "underscore";
-import React, { useState } from "react";
-import "./EightBall.css"
+function EightBall({ answers = ANSWERS }) {
+  // const [color, setColor] = useState("black");
+  // const [msg, setMsg] = useState("Think of a Question");
 
-function EightBall({answers}){
-  const [color, setColor] = useState("black")
-  const [msg, setMsg] = useState("Think of a Question")
+  const [answer, setAnswer] = useState({
+    color: "black",
+    msg: "think of a question"
+  });
 
-  function handleClick(){
-    console.log("I was clicked")
-    const answer = _.sample(answers, [1])
-    setColor(answer[0].color);
-    setMsg(answer[0].msg)
+  function handleClick() {
+    const randomAnswer = _.sample(answers, [1]);
+    // setColor(answer[0].color);
+    // setMsg(answer[0].msg);
+    setAnswer(randomAnswer[0]);
+  }
+
+  function reset(){
+    const resetAnswer = {
+      color: "black",
+      msg: "think of a question"
+    }
+    setAnswer(resetAnswer)
   }
 
   return (
-    <div className="EightBall"
-      onClick={handleClick}
-      style={{"backgroundColor": color}}>
-     <p className="msg">{msg}</p>
+    <div>
+      <div className="EightBall"
+        onClick={handleClick}
+        style={{ "backgroundColor": answer.color }}>
+        <p className="msg">{answer.msg}</p>
+      </div>
+      <button onClick={reset}>Reset</button>
     </div>
-  )
+  );
 }
 
 export default EightBall;
